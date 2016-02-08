b6526ddf90a976fce611cec95ce2c5609eba5ebc
Anil Mujagic <anil.mujagic@gmail.com>
2016-02-08 12:09:27 +0100

    Add snippets for Component

--- SublimeText/JavaScript/README.md
+++ SublimeText/JavaScript/README.md
@@ -4,8 +4,14 @@ Visual Studio Snippets for JavaScript
 Installation
 ------------
 
-Copy *.sublime-snippet files to
+Copy *.sublime-snippet files to:
+Linux
 ```
 ~/.config/sublime-text-3/Packages
 ```
+Windows
+```
+SUBLIME_TEXT_DIR\Data\Packages\User
+```
+
 Adjust path according to your Sublime Text location.
--- VisualStudio/JavaScript/Deploy.bat
+++ VisualStudio/JavaScript/Deploy.bat
@@ -1,9 +1,9 @@
-if exist "%HOMEPATH%\Documents\Visual Studio 2012\Code Snippets\JavaScript\My Code Snippets\" (
-    copy *.snippet "%HOMEPATH%\Documents\Visual Studio 2012\Code Snippets\JavaScript\My Code Snippets\"
+if exist "%SYSTEMDRIVE%%HOMEPATH%\Documents\Visual Studio 2012\Code Snippets\JavaScript\My Code Snippets\" (
+    copy *.snippet "%SYSTEMDRIVE%%HOMEPATH%\Documents\Visual Studio 2012\Code Snippets\JavaScript\My Code Snippets\"
 )
-if exist "%HOMEPATH%\Documents\Visual Studio 2013\Code Snippets\JavaScript\My Code Snippets\" (
-    copy *.snippet "%HOMEPATH%\Documents\Visual Studio 2013\Code Snippets\JavaScript\My Code Snippets\"
+if exist "%SYSTEMDRIVE%%HOMEPATH%\Documents\Visual Studio 2013\Code Snippets\JavaScript\My Code Snippets\" (
+    copy *.snippet "%SYSTEMDRIVE%%HOMEPATH%\Documents\Visual Studio 2013\Code Snippets\JavaScript\My Code Snippets\"
 )
-if exist "%HOMEPATH%\Documents\Visual Studio 2015\Code Snippets\JavaScript\My Code Snippets\" (
-    copy *.snippet "%HOMEPATH%\Documents\Visual Studio 2015\Code Snippets\JavaScript\My Code Snippets\"
+if exist "%SYSTEMDRIVE%%HOMEPATH%\Documents\Visual Studio 2015\Code Snippets\JavaScript\My Code Snippets\" (
+    copy *.snippet "%SYSTEMDRIVE%%HOMEPATH%\Documents\Visual Studio 2015\Code Snippets\JavaScript\My Code Snippets\"
 )
